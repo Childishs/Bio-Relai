58d4a61f89e172b861fcc30e5f8db70cbc1387d4
Mathieu SCHMITZ <mschmitz@geiffel.fr>
2021-10-01 08:57:29 +0200

    suite controleur principal

--- controleurs/controleurPrincipal.php
+++ controleurs/controleurPrincipal.php
@@ -1,7 +1,17 @@
 <?php
 
+
+
 if(isset($_GET['demandeConnexion'])){
     $_SESSION['controleurN1']="connexion";
+}
+
+else if(!isset($_POST['identififcation'])){
+    $SESSSION['identification']['statut']="visiteurs";
+    $SESSSION['controleurBioN1'] = $SESSION['identification']['statut'];
+    $SESSSION['identification']['nom']=null;
+    $SESSSION['identification']['prenom']=null;
+    $SESSSION['identification']['login'] = null;
 }
 else{
     $_SESSION['controleurN1']="visiteurs";
