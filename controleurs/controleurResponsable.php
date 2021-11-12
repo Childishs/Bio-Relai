6c8b79eb655bc21f78aa438919f3881bc900ab42
Flo <chauvain.florian@gmail.com>
2021-11-12 11:43:32 +0100

    control de mat :(

--- controleurs/controleurResponsable.php
+++ controleurs/controleurResponsable.php
@@ -3,10 +3,13 @@
 if($_SESSION['user']['statut'] === "responsable") {
     $menuResponsable = new Menu('Responsable');
     // $menuResponsable->ajouterComposant($menuConnexion->creerItemLien('Connexion','connexion'));
-    $menuResponsable->ajouterComposant($menuResponsable->creerItemLien("Responsable","Responsable"));
+    $menuResponsable->ajouterComposant($menuResponsable->creerItemLien("Home","Responsable"));
+    $menuResponsable->ajouterComposant($menuResponsable->creerItemLien("Compte","Compte"));
+
+
 
     // $menuResponsable = $menuResponsable->creerMenu('0','demandeConnexion');
-    $menu = $menuResponsable->creerMenu('0','Responsable');
+    $menuResponsable->creerMenu('0','Responsable');
 
     require_once('vues/responsable/vueResponsable.php');
 }
--- vues/responsable/vueResponsable.php
+++ vues/responsable/vueResponsable.php
@@ -1,18 +1,25 @@
 <section id="contenueResponsable">
     <header>
         <nav>
-            <?php  var_dump($menu);// $menu->afficherMenu();
-            // $menuConnexion->afficheMenu(); ?>
+            <?php 
+            $menuResponsable->afficherMenu();
+            ?>
 
         </nav>
     </header>
 
     <section>
-        <?php // echo $laListeProducteurs; 
-       
-            
+        <?php 
             echo "Bienvenu à vous Responsable ! Voici vos infos !<br/>" ;
-            echo "Votre magnfique nom de l'amour est :  " . $_SESSION['user']['nom'];
+            echo "Votre magnfique nom de l'amour est :  " . $_SESSION['user']['nom'] . "<br/>";
+            echo "Pour modifier vos informations, cliquez ici <br/>";
+            echo "Pour accéder aux producteurs <br/>";
+            echo "Pour accéder à la gestion de vente en cours <br/>";
+            echo "Pour accéder autorisations de commandes <br/>";
+            echo "Pour accéder aux autorisations de vente <br/>";
+            echo "Ajouter de nouvelles catégories <br/>";
+            echo "Inspecter les factures (clients et producteurs) <br/>";
+            var_dump($_SESSION);
             ?>
     </section>
 </section>
